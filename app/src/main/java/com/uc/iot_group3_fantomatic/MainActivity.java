698cdc7c4afb05dcb1489ee94e6219fbd4b62e76
michaellchandra <chandramichaell93@gmail.com>
2022-05-23 15:50:32 +0700

    testing

--- app/src/main/java/com/uc/iot_group3_fantomatic/MainActivity.java
+++ app/src/main/java/com/uc/iot_group3_fantomatic/MainActivity.java
@@ -3,12 +3,28 @@ package com.uc.iot_group3_fantomatic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView_HelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textView_HelloWorld.setText("haloo");
+
+
+
+
+
+
+        // Find View By ID
+        textView_HelloWorld = findViewById(R.id.textView_HelloWorld);
+
     }
 }
