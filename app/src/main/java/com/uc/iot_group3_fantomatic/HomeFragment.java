2999bea7c1e4db30aeb68bee4b861b8d5311c822
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2022-06-09 00:12:11 +0700

    Update

--- app/src/main/java/com/uc/iot_group3_fantomatic/HomeFragment.java
+++ app/src/main/java/com/uc/iot_group3_fantomatic/HomeFragment.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
-import com.google.android.material.snackbar.Snackbar;
 import com.uc.iot_group3_fantomatic.helper.FanSetting;
 import com.uc.iot_group3_fantomatic.helper.Gate;
 
